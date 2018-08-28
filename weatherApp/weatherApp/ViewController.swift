bb1e64626214c8688e354f7a85cce3065e33935c
Burak Çokyıldırım <burak3883@icloud.com>
2018-08-28 11:09:56 +0300

    Started

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Burak Çokyıldırım on 28.08.2018.
+//  Copyright © 2018 Burak Çokyıldırım. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
